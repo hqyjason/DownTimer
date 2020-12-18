cb08ff2b0915da1baaafa362e3e295e3c3fa6d40
hqyjason <15117904175@163.com>
2020-12-18 14:28:18 +0800

    my second commit

--- app/src/main/java/com/example/hequn/downtimer/MainActivity2.java
+++ app/src/main/java/com/example/hequn/downtimer/MainActivity2.java
@@ -41,6 +41,7 @@ public class MainActivity2 extends AppCompatActivity {
 
             leftTime--;
             Log.e("测试分支" , "yes");
+            Log.e("测试分支1" , "yes1");
             if (leftTime > 0) {
                 //倒计时效果展示
                 String formatLongToTimeStr = formatLongToTimeStr1(leftTime);
